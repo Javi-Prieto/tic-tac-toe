9803849f7f71a42f93315450bbd620c8a2617752
Javier Prieto <jpojaviprior@gmail.com>
2023-12-21 14:25:45 +0100

    finished for test now

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,30 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { GameService } from '../../services/game.service';
+import { ActivatedRoute } from '@angular/router';
+import { QuerySnapshot, onSnapshot } from '@angular/fire/firestore';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
-export class BoardComponent {
-  goesX: boolean = true;
+export class BoardComponent implements OnInit {
   board: string[] = [];
   row = 0; 
-  
-  onClickedBox(event: any) {
-    let ellement = event.target;
-    if(ellement.hasChildNodes()){
-      console.log("nonono")
-    }else{
-      const p = document.createElement("p");
-      p.classList.add('fw-bold', 'display-1');
-      if(this.goesX){
-        p.textContent= 'X';
-        ellement.append(p);
-        this.goesX = false;
+  route: ActivatedRoute = inject(ActivatedRoute);
+  gameId;
+  isWaiting: boolean = false;
+  c1: string = '';
+  c2: string = '';
+  c3: string = '';
+  c4: string = '';
+  c5: string = '';
+  c6: string = '';
+  c7: string = '';
+  c8: string = '';
+  c9: string = '';
+  player1: string = '';
+  player2: string = '';
+  constructor(private gameService: GameService){
+    this.gameId = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit(){
+    let doc = this.gameService.isGameFull(this.gameId);
+    onSnapshot(doc, (doc) => {
+      const game = doc;
+      if(game.get('player2') == 'null'){
+        this.isWaiting= true;
       }else{
-        p.textContent= 'O';
-        ellement.append(p);
-        this.goesX = true;
+        this.isWaiting = false;
       }
-    }
+      this.c1 = game.get('c1');
+      this.c2 = game.get('c2');
+      this.c3 = game.get('c3');
+      this.c4 = game.get('c4');
+      this.c5 = game.get('c5');
+      this.c6 = game.get('c6');
+      this.c7 = game.get('c7');
+      this.c8 = game.get('c8');
+      this.c9 = game.get('c9');
+      this.player1 = game.get('player1');
+      this.player2 = game.get('player2');
+  });
+      
+    
+  }
+
+
+  async onClickedBox(event: any) {
+    let ellement = event.target;
+    let doc = this.gameService.isGameFull(this.gameId);
+    let content: string = ellement.textContent;
+    let canClick:boolean =true;
+    const whoGoes = await this.gameService.whoGoes(this.gameId);
+    let boxClickeId = ellement.id;
+    let boxValue;
+    onSnapshot(doc, (doc) => {
+      console.log(doc.get(boxClickeId));
+      canClick = doc.get(boxClickeId) == ''? true: false;
+    });
+    if(content.length != 0){
+      console.log("nonono");
+    }else{
+      if(localStorage.getItem('PLAYER') == '1'){
+        if(whoGoes){
+          boxValue = 'X';
+          this.gameService.changeTurn(false, this.gameId, boxValue, boxClickeId);
+        }else{
+          alert('Not your turn');
+        };
+      }else{
+        if(!whoGoes){
+          boxValue = 'O';
+          this.gameService.changeTurn(true, this.gameId, boxValue, boxClickeId);
+        }else{
+          alert('Not your turn');
+        }
+        
+      }
+    };
+    this.checkWin(whoGoes);
+  }
+
+  checkWin(whoGoes: boolean){
+    let check123 = this.c1.match(this.c2) && this.c2.match(this.c3);
+    let check456 = this.c4.match(this.c5) && this.c5.match(this.c6);
+    let check789 = this.c7.match(this.c8) && this.c8.match(this.c9);
+    let check147 = this.c1.match(this.c4) && this.c4.match(this.c7);
+    let check258 = this.c2.match(this.c5) && this.c5.match(this.c8);
+    let check369 = this.c3.match(this.c6) && this.c9.match(this.c6);
+    let check159 = this.c1.match(this.c5) && this.c9.match(this.c5);
+    let check357 = this.c3.match(this.c5) && this.c7.match(this.c5);
+    
+    if(check123 || check456 || check789 || check147 || check258 || check369 || check159 || check357){
+      if(whoGoes){
+        alert(this.player1 + 'WINS!!!!!!!!');
+      }else{
+        alert(this.player2 + 'WINS!!!!!!!!');
+      };
+    };
+
+    
   }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, addDoc, collection, doc, getDocs, limit, query, updateDoc, where } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, doc, getDoc, getDocs, limit, onSnapshot, query, updateDoc, where } from '@angular/fire/firestore';
 
 
 const COLLECTION = 'games';
@@ -20,11 +20,10 @@ export class GameService {
   async addPlayer(name:string){
     let docs = await this.getEmpty2PlayersGames();
     let selectedGameRef;
-    let toReturn: {game:any, isNew:boolean};
     if(!docs.empty){
        selectedGameRef = doc(this.firestore, COLLECTION, docs.docs[0].id);
-       updateDoc(selectedGameRef, {player2: name});
-       toReturn = {game: selectedGameRef, isNew: false};
+       updateDoc(selectedGameRef, {player2: name, goesX: true});
+       localStorage.setItem('PLAYER', '2');
     }else{
       selectedGameRef = addDoc(this.gamesRef,{
         c1: '',
@@ -36,13 +35,57 @@ export class GameService {
         c7: '',
         c8: "",
         c9: "",
+        goesX: true,
         player1: name,
         player2: 'null',
         winner: 'null' 
       });
-      toReturn = {game: selectedGameRef, isNew: false};
+      
+      localStorage.setItem('PLAYER', '1');
     };
     
     return selectedGameRef;
+  }
+
+  isGameFull(id: string){
+    return doc(this.firestore, COLLECTION, id);
+  }
+
+  changeTurn(goesX: boolean ,id:string, value:string, _cId: string){
+    let selectedGameRef = doc(this.firestore, COLLECTION, id);
+    switch (_cId){
+      case 'c1':
+        updateDoc(selectedGameRef, {goesX: goesX, c1: value});
+        break;
+      case 'c2':
+        updateDoc(selectedGameRef, {goesX: goesX, c2: value});
+        break;
+      case 'c3':
+        updateDoc(selectedGameRef, {goesX: goesX, c3: value});
+        break;
+      case 'c4':
+        updateDoc(selectedGameRef, {goesX: goesX, c4: value});
+        break;
+      case 'c5':
+        updateDoc(selectedGameRef, {goesX: goesX, c5: value});
+        break;
+      case 'c6':
+        updateDoc(selectedGameRef, {goesX: goesX, c6: value});
+        break;
+      case 'c7':
+        updateDoc(selectedGameRef, {goesX: goesX, c7: value});
+        break;
+      case 'c8':
+        updateDoc(selectedGameRef, {goesX: goesX, c8: value});
+        break;
+      case 'c9':
+        updateDoc(selectedGameRef, {goesX: goesX, c9: value});
+        break;
+      }
+    
+  }
+  async whoGoes(id: string){
+    let selectedGame = await getDoc(doc(this.firestore, COLLECTION, id));
+    return selectedGame.get('goesX');
   }
 }
